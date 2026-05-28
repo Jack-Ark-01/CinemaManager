2ad49d0e4142df5feffdf521614015d847898f24
fra-inteso <france060902@gmail.com>
2026-05-28 12:02:54 +0200

    Aggiunta classi ProiezioneStandard e Proiezione3D

--- src/com/cinemamanager/models/Proiezione3D.java
+++ src/com/cinemamanager/models/Proiezione3D.java
@@ -1,5 +1,65 @@
 package com.cinemamanager.models;
 
-public class Proiezione3D {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Set;
 
+public class Proiezione3D extends Proiezione{
+	
+	
+	private double supplemento3D;
+	private boolean occhialiInclusi;
+	
+	
+	public Proiezione3D(int id, Sala sala, Film film, LocalDate data, LocalTime oraInizio, double prezzobase,
+			Set<String> tag) {
+		super(id, sala, film, data, oraInizio, prezzobase, tag);
+		setSupplemento3D(prezzobase);
+		setOcchialiInclusi(occhialiInclusi);
+		
+	}
+	
+	@Override
+	public LocalDateTime getDataOraInizio() {
+		return null;
+	}
+
+	@Override
+	public double calcolaPrezzoFinale() {
+		
+		double prezzoFinale;
+		
+		if(contieneTag("3d")) {
+			prezzoFinale= getPrezzoBase()+supplemento3D;//suppleento 3d
+			if(isNelWeekEnd()) prezzoFinale+=2.00;
+			if (occhialiInclusi)prezzoFinale+=1.00;
+			return prezzoFinale;
+			
+		}
+		else {
+			System.out.println("errore la sala proiezione 3d non è supportata");
+			return -1;
+		}
+	}
+
+	public double getSupplemento3D() {
+		return supplemento3D;
+	}
+
+	public void setSupplemento3D(double supplemento3d) {
+		supplemento3D = supplemento3d;
+	}
+
+	public boolean isOcchialiInclusi() {
+		return occhialiInclusi;
+	}
+
+	public void setOcchialiInclusi(boolean occhialiInclusi) {
+		this.occhialiInclusi = occhialiInclusi;
+	}
+	@Override
+	public String getTipoProiezione() {
+		return "proiezione 3d";
+	}
 }
--- src/com/cinemamanager/models/ProiezioneStandard.java
+++ src/com/cinemamanager/models/ProiezioneStandard.java
@@ -1,5 +1,43 @@
 package com.cinemamanager.models;
 
-public class ProiezioneStandard {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Set;
+
+public class ProiezioneStandard extends Proiezione{
+
+	public ProiezioneStandard(int id, Sala sala, Film film, LocalDate data, LocalTime oraInizio, double prezzobase,
+			Set<String> tag) {
+		super(id, sala, film, data, oraInizio, prezzobase, tag);
+		
+	}
+	
+	
+	
+	
+	@Override
+	public LocalDateTime getDataOraInizio() {
+		
+		return LocalDateTime.of(getData(), getOraInizio());
+	}
+
+	@Override
+	public double calcolaPrezzoFinale() {
+		double prezzoFinale;
+		prezzoFinale= getPrezzoBase();
+		if(isNelWeekEnd()) prezzoFinale+=2.00;
+		if(isSerale()) prezzoFinale+=2.00;
+		return prezzoFinale;
+	}
+
+
+	@Override
+	public String getTipoProiezione() {
+		return "proiezione standard";
+	}
+	
+	
+
 
 }
